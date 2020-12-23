3492c5cc694fff2cfd65ca43ff89fbd93cb10f13
Tanmay Basu <mailtanmaybasu@gmail.com>
2020-12-23 23:59:02 +0000

    Update data_extraction.py

--- data_extraction.py
+++ data_extraction.py
@@ -7,6 +7,7 @@ Created on Tuesday, December 22, 2020 @ 19:45:28
 
 import csv,os,re,sys
 import fitz
+import nltk
 from nltk import tokenize
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import CountVectorizer
@@ -19,13 +20,81 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_selection import SelectKBest,chi2,mutual_info_classif 
 
-stopset = set(stopwords.words('english'))
 
 class data_extraction():
      def __init__(self,path='/home/data_extrcation/'):
         self.path = path
+    # Text refinement
+     def text_refinement(self,text='hello'):
+        text = re.sub(r'[^!"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~\n\w]+',' ', text)      # Remove special characters e.g., emoticons-😄. The ^ in the beginning ensures that all the natural characters will be kept. 
+#        text = re.sub(r'[^a-zA-Z0-9.?:!$\n]', ' ', text)                          # Remove special characters
+        text=re.sub(r'[?]', '.', text)                                             # Replace '?' with '.' to properly identify floating point numbers 
+        text=re.sub(r'([a-zA-Z0-9])([\),.!?;-]+)([a-zA-Z])', r'\1\2 \3', text )    # Space between delimmiter and letter   
+        text=re.sub(r'([a-z])([\.])([\s]*)([a-z])', r'\1 \3\4', text)              # Reomove '.' between two lowercase letters e.g., et al. xxx
+        text=re.sub(r'([a-z])([\.]*)([0-9])', r'\1\2 \3', text)                    # Space between letter and no.    
+        text=re.sub(r'(\s)([a-z0-9]+)([A-Z])([\w]+)', r'\1\2. \3\4', text)         # Put a '.' after a lowercase letter/number followed by Uppercase e.g., drains removed by day threeHe continued to 
+        text=re.sub(r'(\.)([\s]*)([\.]+)', r'\1', text)                            # Removing extra '.'s, if any   
+
+        return text
+           
+    # check if a sentence is relevant to a keyphrase 
+     def check_relevant_sentences(self,sentence,keyphrases):
+        sentence = re.sub(r'[^a-zA-Z0-9.?:!$\n]', ' ', sentence)    # Remove special character 
+        for phrase in keyphrases:
+            if sentence.find(phrase)>=0 and re.findall(r'\d+\.\d+', sentence)!=[]:
+                return 1
+        return 0
+    
+    # Building training corpus  
+     def build_training_data(self):
+        os.system('ls '+self.path+'training_data/*.pdf> '+self.path+'list_training_data.txt')
+        fl=open(self.path+'list_training_data.txt', "r") 
+        trn_files = list(csv.reader(fl,delimiter='\n')) 
+        trn_files=[item for sublist in trn_files for item in sublist]
+        fl.close()
+    
+        fk=open(self.path+'keyphrases.txt', "r") 
+        keyphrases = list(csv.reader(fk,delimiter='\n'))
+        keyphrases = [item for sublist in keyphrases for item in sublist]
+        fk.close() 
+        count=0;
+        print('############### Preparing Training Data ############### \n')
+        fp=open(self.path+'training_relevant_class_data.csv',"w")
+        fn=open(self.path+'training_irrelevant_class_data.csv',"w")  
+        for item in trn_files:
+            count+=1
+            try:
+              doc = fitz.open(item, filetype = "pdf")        # PDF to text conversion  
+              texts=[] 
+              for page in doc:
+                 texts.append(page.getText().encode("utf8"))    
+              try:
+                  text=b''.join(texts)                       # If Fitz return the text in binary mode 
+                  text=text.decode('utf-8')
+              except:
+                  text=''.join(texts)
+            except:
+              text=''                  
+            text=self.text_refinement(text)                     # Cleaning text file 
+            fp.write('trn '+str(count)+',')
+            fn.write(str(count)+',')
+            text=re.sub(r',', r';', text)      # replacing , by ; to build the csv properly 
+            text=re.sub(r'\n', r' ', text)    # replacing \n by ; to build the csv properly
+            sentences=nltk.sent_tokenize(text)
+            for sentence in sentences:
+                if sentence!='':
+                    phrase_score=self.check_relevant_sentences(sentence,keyphrases)
+                    if phrase_score==1:
+                        fp.write(sentence+' ')
+                    elif phrase_score==0 and re.findall(r'\d+', sentence)==[]:
+                        fn.write(sentence+' ') 
+            fp.write('\n')
+            fn.write('\n')
+        fp.close()
+        fn.close()
+        
     # Selection of classifiers  
-     def classifier(self,opt,no_term,trn_data,trn_cat):        
+     def classification_pipeline(self,opt,no_term,trn_data,trn_cat):        
         # Logistic Regression 
         if opt=='lr':
             print('\n\t### Classification of given texts using Logistic Regression Classifier ### \n')
@@ -101,7 +170,7 @@ class data_extraction():
         return clf,ext2
     
     # Classification using the Gold Statndard after creating it from the raw text    
-     def classify(self):
+     def data_extraction(self):
         opt = input("Choose a classifier : \n\n\t 'lr' to select Logistic Regression" 
                        "\n\t 'r' to select Random Forest" 
                        "\n\t 'ls' to select Linear SVC" 
@@ -142,7 +211,7 @@ class data_extraction():
                     trn_cat.append(1)
                     p2=p2+1
     # Classification pipeline                   
-        clf,ext2=self.classifier(opt,no_term,trn_data,trn_cat)   
+        clf,ext2=self.classification_pipeline(opt,no_term,trn_data,trn_cat)   
     # Classification of the test samples  
         os.system('ls '+self.path+'test_data/*.pdf>'+self.path+'list_test_data.txt')
         fi=open(self.path+'list_test_data.txt', "r") 
@@ -167,15 +236,7 @@ class data_extraction():
             except:
                 text=''                  
             # Preparing Test Samples 
-#            text=build_training_data.text_refinement(text) 
-            text=re.sub(r'[?]', '.', text)                                             # Replace '?' with '.' to properly identify floating point numbers 
-            text = re.sub(r'[^!"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~\n\w]+',' ', text)      # Remove special characters e.g., emoticons-😄. The ^ in the beginning ensures that all the natural characters will be kept. 
-            text=re.sub(r'([a-zA-Z0-9])([\),.!?;-]+)([a-zA-Z])', r'\1\2 \3', text )    # Space between delimmiter and letter   
-            text=re.sub(r'([a-z])([\.])([\s]*)([a-z])', r'\1 \3\4', text)              # Reomove '.' between two lowercase letters e.g., et al. xxx
-            text=re.sub(r'([a-z])([\.]*)([0-9])', r'\1\2 \3', text)                    # Space between letter and no.    
-            text=re.sub(r'(\s)([a-z0-9]+)([A-Z])([\w]+)', r'\1\2. \3\4', text)         # Put a '.' after a lowercase letter/number followed by Uppercase e.g., drains removed by day threeHe continued to 
-            text=re.sub(r'(\.)([\s]*)([\.]+)', r'\1', text)                            # Removing extra '.'s, if any      
-            text=re.sub(r'\n', r'', text)                                              # Removing \n 
+            text=self.text_refinement(text)                     # Cleaning text file 
             if text!='':
                 sentences = tokenize.sent_tokenize(text)
                 for sentence in sentences:                       # Extracting sentences
@@ -188,24 +249,19 @@ class data_extraction():
                 out.write('The relevant sentences are as follow: \n')
             # Classification
                 predicted = clf.predict(tst_data) 
+                print(predicted)
                 nps=0
                 for i in range(0,len(predicted)):
-                    if predicted[i] == 0 and re.findall(r'\d+\.\d+', tst_data[i])!=[]:
+ # Check if there is a number and the term 'SD'
+#                    if predicted[i] == 0 and re.findall(r'\d+', sentence)!=[]: 
+                    if predicted[i] == 0:
                         nps=nps+1                 
-                        tst_data[i]=re.sub(r'(\d+\.\d+°*)(\s*\�\s*)(\d+\.\d+)', r'\1 ± \3',tst_data[i]) # SHIFT+OPTION+PLUS Sign - plus-minus symbol
                         out.write('\n'+str(nps)+")  "+tst_data[i]+'\n')               
                 print("Total No. of Relevant Sentences of Test Sample"+str(count)+" : %d\n" %nps)
             else:
                 out.write('Test file '+str(count)+'is empty \n') 
-                print('Test file '+str(count)+'is empty \n')
+                print('Test file '+str(count)+' is empty \n')
         print('No of sentences belong to RELEVANT class of the training corpus: '+ str(p1)) 
         print('No of sentences belong to IRRELEVANT class of the training corpus: '+ str(p2)) 
         print('No of sentences belong to the TEST corpus: '+ str(p3)) 
-    
-
-    
-    
-    
-    
-    
     
